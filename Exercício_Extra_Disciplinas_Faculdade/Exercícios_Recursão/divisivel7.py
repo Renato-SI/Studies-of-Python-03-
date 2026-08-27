f917401fdedf9976e5683a6051d01a1c0fa8858c
Renato-SI <renatorrbf@gmail.com>
2026-08-27 17:10:13 -0300

    refactor and Add: Função para calcular o tempo dos algoritmos recursivos(caso do divisivel7.py)

--- Exercício_Extra_Disciplinas_Faculdade/Exercícios_Recursão/divisivel7.py
+++ Exercício_Extra_Disciplinas_Faculdade/Exercícios_Recursão/divisivel7.py
@@ -19,16 +19,23 @@ Restrições:
 """
 
 entrada = int(input(""))
-
 def divisivel7(numero):
     if numero == 7 or numero == 49:
         return "s"
+    
     elif numero < 14:
         return "n"
-    else: 
-        last_digit = numero % 10
-        remaining_digits = numero // 10
-        mulandsum = (last_digit * 5) + remaining_digits
-        return divisivel7(mulandsum)
+    last_digit = numero % 10
+    remaining_digits = numero // 10
+    new_number = (last_digit * 5) + remaining_digits
+    return divisivel7(new_number)
     
-print(divisivel7(entrada))
+
+def tempo(n):
+    import time
+    start = time.time()
+    result = divisivel7(n)
+    end = time.time()
+    return f"A função divisivel7({n})= {result}, rodou em: {end - start:.10f}"
+
+print(tempo(1000213123010321212121212172816261638136183131))
