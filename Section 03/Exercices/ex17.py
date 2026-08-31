07c9070cc4f12787a95b5818126c147d9cc00090
Renato-SI <renatorrbf@gmail.com>
2026-08-31 12:29:27 -0300

    Add: ex17, empacotando e desempacotando dados de uma lista e exibindo os index

--- Section 03/Exercices/ex17.py
+++ Section 03/Exercices/ex17.py
@@ -1,7 +1,25 @@
 """
 Exercício
 Exiba os índices da lista
-0 Maria
-1 Helena
-2 Luiz
+0 Renato
+1 Anna Nery
+2 Luciana
 """
+
+lista = ['Renato', 'Anna Nery', 'Luciana']
+# Se eu quiser mexer/ adicionar ou removger valores, nesse caso t5enho q fazer antes da variavel indice
+# No caso Abaixo eu posso adicionar e remover sem me 'preocupar' com isso já que fiz da forma direta.
+lista.append('Simba')
+lista.append('Art')
+
+# lista.pop
+indice = range(len(lista))
+
+print('=== METODO \'INDIRETO\' ===')
+for index in indice:
+    print(index, lista[index])
+
+# OU de maneiraa direta!
+print('=== METODO DIRETO ===')
+for index in range(len(lista)):
+    print(index, lista[index])
