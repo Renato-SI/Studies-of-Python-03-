9be4f9a0d81f85ebbd5c15d2362dc9713b4bf065
Renato-SI <renatorrbf@gmail.com>
2026-09-17 12:30:06 -0300

    refactor

--- Exercício_Extra_Disciplinas/faculdade/casamento_HTML.py
+++ Exercício_Extra_Disciplinas/faculdade/casamento_HTML.py
@@ -1,15 +1,4 @@
-"""
-Crie uma função que receba uma string contendo tags HTML e verifique se todas as tags estão corretamente ab ertas e fechadas.
-Utilize uma pilha (stack) para controlar as tags abertas. Sempre que uma tag de abertura for encontrada, ela deve ser adicionada à pilha.
-Quando uma tag de fechamento for encontrada, ela deve corresponder à tag que está no topo da pilha.
-A função deve retornar True caso todas as tags estejam corretamente fechadas e False caso exista algum a tag sem fechamento, 
-uma tag fechada fora de ordem ou uma tag de fechamento sem uma abertura correspondente.
-Saída esperada:
-'<div<p>Texto</p></div>' = False
-'</div>' =  False
-'<html><body><h1>Hello</h1></body></html>' = True
-'<div><p>Olá</p></div>' = True
-"""
+
 
 def verificar_html(entrada):
     stack = []
@@ -35,7 +24,7 @@ def verificar_html(entrada):
                 # Se o topo da pilha for diferente da tag de fechamento que ele achou == False
                 if topo != tag[1:]:
                     return False
-            # Evito percorrer os elementos da tag novamente    
+                
             i = fim_tag
 
         i += 1
